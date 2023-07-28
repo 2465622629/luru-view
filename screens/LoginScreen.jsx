fae47072a27247c41d30fd9b2c1438ff9615c6d9
2465622629 <2465622629@qq.com>
2023-07-28 20:45:30 +0800

    功能：登录页面添加登录功能

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Animated, StyleSheet, Text, TextInput, TouchableOpacity, View,Image } from 'react-native';
+import { Animated, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native';
 import axiosInstance from "../utils/comreqtool";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import JwtHelper from "../utils/jwtUtils";
@@ -62,110 +62,113 @@ export default function LoginScreen() {
         }
     };
 
-    const loginButtonScale = loginButtonAnim.interpolate({
-        inputRange: [0, 1],
-        outputRange: [1, 0.9],
-    });
-
-    const registerButtonScale = registerButtonAnim.interpolate({
-        inputRange: [0, 1],
-        outputRange: [1, 0.9],
-    });
 
     return (
         <View style={styles.container}>
-          <Image
-            source={{uri:backgroundImageUrl}}
-            style={styles.logo}
-            resizeMode="contain"
-          />
-          <Text style={styles.heading}>欢迎登录贝壳联盟</Text>
-          <Text style={styles.subheading}>请输入您的登录信息</Text>
-    
-          <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.input}
-              placeholder="用户名"
-              keyboardType="email-address"
+            <Image
+                source={{ uri: backgroundImageUrl }}
+                style={styles.logo}
+                resizeMode="contain"
             />
-            <TextInput
-              style={styles.input}
-              placeholder="密码"
-              secureTextEntry
-            />
-          </View>
-    
-          <TouchableOpacity style={styles.loginButton}>
-            <Text style={styles.loginButtonText}>登录</Text>
-          </TouchableOpacity>
-    
-          <TouchableOpacity style={styles.registerButton}>
-            <Text style={styles.registerButtonText}>注册</Text>
-          </TouchableOpacity>
+            <Text style={styles.heading}>欢迎登录贝壳联盟</Text>
+            <Text style={styles.subheading}>请输入您的登录信息</Text>
+
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="用户名"
+                    keyboardType="email-address"
+                    autoCapitalize="none" // 不自动大写
+                    value={username}
+                    onChangeText={setUsername}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="密码"
+                    secureTextEntry
+                    autoCapitalize="none" // 不自动大写
+                    value={password}
+                    onChangeText={setPassword}
+                />
+            </View>
+
+            <TouchableOpacity
+                style={styles.loginButton}
+                onPress={handleLogin}
+            >
+                <Text style={styles.loginButtonText}>登录</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={styles.registerButton}
+                onPress={handleRegister}
+            >
+                <Text style={styles.registerButtonText}>注册</Text>
+            </TouchableOpacity>
         </View>
-      );
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 20,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
     },
     logo: {
-      width: 150,
-      height: 150,
+        width: 150,
+        height: 150,
     },
     heading: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginTop: 20,
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 20,
     },
     subheading: {
-      fontSize: 16,
-      color: '#888',
-      marginTop: 5,
-      marginBottom: 20,
+        fontSize: 16,
+        color: '#888',
+        marginTop: 5,
+        marginBottom: 20,
     },
     inputContainer: {
-      width: '100%',
-      marginBottom: 20,
+        width: '100%',
+        marginBottom: 20,
     },
     input: {
-      height: 50,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      borderRadius: 10,
-      paddingLeft: 15,
-      marginBottom: 10,
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 10,
+        paddingLeft: 15,
+        marginBottom: 10,
     },
     loginButton: {
-      backgroundColor: '#5468ff',
-      borderRadius: 10,
-      width: '100%',
-      height: 50,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginBottom: 10,
+        backgroundColor: '#5468ff',
+        borderRadius: 10,
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
     },
     loginButtonText: {
-      color: 'white',
-      fontSize: 18,
-      fontWeight: 'bold',
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
     },
     registerButton: {
-      borderWidth: 1,
-      borderColor: '#5468ff',
-      borderRadius: 10,
-      width: '100%',
-      height: 50,
-      justifyContent: 'center',
-      alignItems: 'center',
+        borderWidth: 1,
+        borderColor: '#5468ff',
+        borderRadius: 10,
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     registerButtonText: {
-      color: '#5468ff',
-      fontSize: 18,
-      fontWeight: 'bold',
+        color: '#5468ff',
+        fontSize: 18,
+        fontWeight: 'bold',
     },
-  });
+});
